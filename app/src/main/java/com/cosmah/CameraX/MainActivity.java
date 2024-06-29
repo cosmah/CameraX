19c78b299b4b65853324b98867c1df2f9c7fe110
cosmah <cosmahke4@gmail.com>
2024-06-29 20:56:27 +0300

    setting up permissions

--- app/src/main/java/com/cosmah/CameraX/MainActivity.java
+++ app/src/main/java/com/cosmah/CameraX/MainActivity.java
@@ -1,12 +1,17 @@
 package com.cosmah.CameraX;
 
+
+import android.Manifest;
+
+import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +20,66 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+    }
+
+    //CAMERA PERMISSION
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.CAMERA
+    };
+
+    private static final int REQUEST_PERMISSIONS = 34;
+
+    //storage permission
+    private  static final int PERMISSIONS_COUNT = 1;
+
+    //CHECK IF PERMISSION DENIED
+    @SuppressLint("NewApi")
+    private boolean arePermissionsDenied(){
+        for (int i = 0; i < PERMISSIONS_COUNT; i++){
+
+                if (checkSelfPermission(PERMISSIONS[i])!= PackageManager.PERMISSION_GRANTED){
+                    return true;
+                }
+
+        }
+        return false;
+    };
+
+    //REQUEST PERMISSION
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permission,
+                                          int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permission, grantResults);
+
+        if (requestCode == REQUEST_PERMISSIONS && grantResults.length > 0){
+            if (arePermissionsDenied()){
+                ((ActivityManager)(this.getSystemService(ACTIVITY_SERVICE))).clearApplicationUserData();
+                recreate();
+            }else {
+                onResume();
+            }
+        }
+    }
+
+    private boolean isCameraInitialized;
+
+    private Camera nCamera = null;
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //CHECK IF ANDROID VERSION IS SAME OR GRETER THAN ANDROID MASHMELLOW
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && arePermissionsDenied()){
+            //ASK FOR PERMISSION
+            requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
+            return;
+        }
+
+        if (!isCameraInitialized){
+
+        }
     }
 }
